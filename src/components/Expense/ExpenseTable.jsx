0fdb1038acdb6cca077f74fc2ab40cfb87e99d6f
Ajay Sahani <sajay3159@gmail.com>
2025-12-12 14:41:49 +0530

    add pagination functionality with next/previous controls

--- src/components/Expense/ExpenseTable.jsx
+++ src/components/Expense/ExpenseTable.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button, Spinner, Table } from "react-bootstrap";
 
 const ExpenseTable = ({
@@ -10,7 +11,13 @@ const ExpenseTable = ({
     (sum, exp) => sum + Number(exp.amount || 0),
     0
   );
-  console.log("totalExpense", totalExpense);
+
+  const [currPage, setCurrPage] = useState(1);
+  const itemPerPage = 3;
+
+  const startIndex = (currPage - 1) * itemPerPage;
+  const expenseData = expenses.slice(startIndex, startIndex + itemPerPage);
+
   const downloadCSV = () => {
     if (!expenses.length) return;
 
@@ -73,7 +80,7 @@ const ExpenseTable = ({
           </tr>
         </thead>
         <tbody>
-          {expenses.map((exp, idx) => (
+          {expenseData.map((exp, idx) => (
             <tr key={exp.id}>
               <td>{idx + 1}</td>
               <td>{exp.amount}</td>
@@ -101,8 +108,36 @@ const ExpenseTable = ({
               Total Spent: ₹{totalExpense}
             </td>
           </tr>
+          <tr>
+            <td colSpan="5" className="text-end fw-bold"></td>
+          </tr>
         </tbody>
       </Table>
+      <div
+        style={{
+          marginTop: "20px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
+        <Button
+          onClick={() => setCurrPage((p) => p - 1)}
+          disabled={currPage === 1}
+        >
+          Previous
+        </Button>
+        <span>
+          Page{currPage} of {Math.ceil(expenses.length / itemPerPage)}
+        </span>
+        <Button
+          onClick={() => setCurrPage((p) => p + 1)}
+          disabled={startIndex + itemPerPage >= expenses.length}
+        >
+          Next
+        </Button>
+      </div>
     </>
   );
 };
